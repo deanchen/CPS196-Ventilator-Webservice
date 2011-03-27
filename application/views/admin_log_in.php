63647f8a4a0a9b5968c1b261aae491348eb1a9b5
unknown <Joe@.(none)>
2011-03-27 18:09:37 -0400

    a

--- application/views/admin_log_in.php
+++ application/views/admin_log_in.php
@@ -4,7 +4,7 @@
 	</head>
 <body>
 <h2>Please Log In To Edit the Survey</h2>	
-<form action="../admin/" method="post">
+<form action="../../admin/" method="post">
 	<label for="password">Password:</label>&nbsp;<input type="password" name="password" />
 	<br /><br /><input type="submit" value="Log In" name="log_in" />	
 </form>
