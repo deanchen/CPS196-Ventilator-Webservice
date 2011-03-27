96e46a1e4984f4a3823df332f5da5c020e627928
Dean Chen <dean@ocirs.com>
2011-03-27 00:26:38 -0400

    Restify/unify completed and question update api.

--- application/controllers/rest.php
+++ application/controllers/rest.php
@@ -12,23 +12,7 @@ class Rest extends CI_Controller {
 		$this->load->view('api_docs');
 	}
 	
-  function action()
-  {
-  	$callback = $this->input->get('callback', NULL);
-    $data = null;
-    $this->load->model('survey');
-	
-	if($this->input->post('action') == 'survey_completed')
-	{
-		$data = $this->survey->setSurveyCompleted($this->input->post('session_id'));
-	}
-	elseif($this->input->post('action') == 'update_survey_answer')
-	{
-		$data = $this->survey->setSurveyResult($this->input->post('session_id'),$this->input->post('question_id'),$this->input->post('selected_option_id'));
-	}
-    $this->load->view('rest',array('callback'=>$callback, 'data' => $data));
-  }
-	
+
   function patient($sessionID)
   {
   	$callback = $this->input->get('callback', NULL);
@@ -67,7 +51,15 @@ class Rest extends CI_Controller {
     }
     elseif($command =='questions')
     {
-      $data = $this->survey->getSurveyQuestions($param1,$param2);
+      if ($this->input->post('selected_option_id') != '') {
+      	$question_id = $param1;
+		$session_id = $param2;
+      	$data = $this->survey->setSurveyResult($session_id,$question_id,$this->input->post('selected_option_id'));
+	  } else {
+	  	$question_group_id = $param1;
+		$session_id = $param2;
+      	$data = $this->survey->getSurveyQuestions($question_group_id,$session_id);
+	  }
     }
     elseif($command =='patient')
     {
